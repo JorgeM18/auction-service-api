5933b146cd9f2852bdae087539c914eafc2b473b
Jorge-Malo <Jorge.Malo@Outsourcing.Libertycolombia.com>
2021-03-22 14:40:39 -0500

    Initial project structure

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-export const hello = async (event) => {
+export const helloLambda = async (event) => {
+  console.log(event);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "hello world from lambda and the serverless framework!" }),
